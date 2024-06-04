237b5839ee606452914f028c5caa0f5d13c4cffd
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-06-05 00:09:55 +0800

    somewhat fixed

--- src/hooks/useOrderService.tsx
+++ src/hooks/useOrderService.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import createSupabaseBrowserClient from "@/lib/supabase/client";
 import { useState } from "react";
 import { useLocalStorage } from "./useLocalStorage";
